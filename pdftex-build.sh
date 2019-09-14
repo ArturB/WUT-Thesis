85340e019535d7eb1e88a8deea33615e844a79f8
Artur Brodzki <artur@brodzki.org>
2019-09-14 22:15:43 +0200

    add admin option to tex

--- pdftex-build.sh
+++ pdftex-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pdflatex -synctex=1 -interaction=nonstopmode "main".tex
+pdflatex -synctex=1 -interaction=nonstopmode "main".tex @$
 bibtex "main"
-pdflatex -synctex=1 -interaction=nonstopmode "main".tex
-pdflatex -synctex=1 -interaction=nonstopmode "main".tex
+pdflatex -synctex=1 -interaction=nonstopmode "main".tex @$
+pdflatex -synctex=1 -interaction=nonstopmode "main".tex @$

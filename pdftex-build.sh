03b082ac39b31ba37e8bf5815b04aeed01626db9
Artur Brodzki <artur@brodzki.org>
2019-09-14 22:51:15 +0200

    cat

--- pdftex-build.sh
+++ pdftex-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pdflatex -synctex=1 -interaction=nonstopmode --admin "main".tex
-bibtex --admin "main"
-pdflatex -synctex=1 -interaction=nonstopmode --admin "main".tex
-pdflatex -synctex=1 -interaction=nonstopmode --admin "main".tex
+pdflatex -synctex=1 -interaction=nonstopmode "main".tex
+bibtex "main"
+pdflatex -synctex=1 -interaction=nonstopmode "main".tex
+pdflatex -synctex=1 -interaction=nonstopmode "main".tex

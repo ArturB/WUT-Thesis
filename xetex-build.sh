ee864200f3aeb7fa49be1dfae8051268801f0001
Artur Brodzki <artur@brodzki.org>
2019-09-14 22:40:16 +0200

    admin change

--- xetex-build.sh
+++ xetex-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xelatex -synctex=1 -interaction=nonstopmode $@ "main".tex
-bibtex $@ "main"
-xelatex -synctex=1 -interaction=nonstopmode $@ "main".tex
-xelatex -synctex=1 -interaction=nonstopmode $@ "main".tex
+xelatex -synctex=1 -interaction=nonstopmode --admin "main".tex
+bibtex --admin "main"
+xelatex -synctex=1 -interaction=nonstopmode --admin "main".tex
+xelatex -synctex=1 -interaction=nonstopmode --admin "main".tex

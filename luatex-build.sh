3e011a68018f89efb0fb5cbf46d262890460bfc0
Artur Brodzki <artur@brodzki.org>
2019-09-14 22:23:31 +0200

    array typo fix

--- luatex-build.sh
+++ luatex-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-lualatex -synctex=1 -interaction=nonstopmode "main".tex @$
+lualatex -synctex=1 -interaction=nonstopmode "main".tex $@
 bibtex "main"
-lualatex -synctex=1 -interaction=nonstopmode "main".tex @$
-lualatex -synctex=1 -interaction=nonstopmode "main".tex @$
+lualatex -synctex=1 -interaction=nonstopmode "main".tex $@
+lualatex -synctex=1 -interaction=nonstopmode "main".tex $@

9247359519a6207424cf0548130ef5916abf5d31
Artur M. Brodzki <artur@brodzki.org>
2021-01-20 20:23:18 +0100

    verify-pdf

--- scripts/verify-build.sh
+++ scripts/verify-build.sh
@@ -6,23 +6,29 @@ case $1 in
     lua)
         pdftotext ./pdfs/texlive-lualatex.pdf ./pdfs/texlive-lualatex.txt
         cat ./pdfs/texlive-lualatex.txt | tr -d "[:space:]" > ./pdfs/texlive-lualatex_tr.txt
-        cat ./pdfs/texlive-lualatex_tr.txt
+        echo "Generated pdf:"
         echo "----------------------"
-        #diff ./pdfs/texlive-lualatex_tr.txt ./releases/referential/referential-lualatex.txt
+        cat ./pdfs/texlive-lualatex_tr.txt
+        echo "\n----------------------"
+        diff ./pdfs/texlive-lualatex_tr.txt ./releases/referential/referential-lualatex.txt
         ;;
     pdf)
         pdftotext ./pdfs/texlive-pdflatex.pdf ./pdfs/texlive-pdflatex.txt
         cat ./pdfs/texlive-pdflatex.txt | tr -d "[:space:]" > ./pdfs/texlive-pdflatex_tr.txt
-        cat ./pdfs/texlive-pdflatex_tr.txt
+        echo "Generated pdf:"
         echo "----------------------"
-        #diff ./pdfs/texlive-pdflatex_tr.txt ./releases/referential/referential-pdflatex.txt
+        cat ./pdfs/texlive-pdflatex_tr.txt
+        echo "\n----------------------"
+        diff ./pdfs/texlive-pdflatex_tr.txt ./releases/referential/referential-pdflatex.txt
         ;;
     xetex)
         pdftotext ./pdfs/texlive-xelatex.pdf ./pdfs/texlive-xelatex.txt
         cat ./pdfs/texlive-xelatex.txt | tr -d "[:space:]" > ./pdfs/texlive-xelatex_tr.txt
-        cat ./pdfs/texlive-xelatex_tr.txt
+        echo "Generated pdf:"
         echo "----------------------"
-        #diff ./pdfs/texlive-xelatex_tr.txt ./releases/referential/referential-xelatex.txt
+        cat ./pdfs/texlive-xelatex_tr.txt
+        echo "\n----------------------"
+        diff ./pdfs/texlive-xelatex_tr.txt ./releases/referential/referential-xelatex.txt
         ;;
     *)
         echo Unknown LaTeX compiler!

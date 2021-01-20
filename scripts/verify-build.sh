9efb414b462bc86fa9b9b25c5470c5029608ae10
Artur M. Brodzki <artur@brodzki.org>
2021-01-20 20:30:44 +0100

    verify-build additional messages

--- scripts/verify-build.sh
+++ scripts/verify-build.sh
@@ -3,35 +3,46 @@
 set -e
 
 case $1 in
+
     lua)
         pdftotext ./pdfs/texlive-lualatex.pdf ./pdfs/texlive-lualatex.txt
         cat ./pdfs/texlive-lualatex.txt | tr -d "[:space:]" > ./pdfs/texlive-lualatex_tr.txt
         echo "Generated pdf:"
         echo "----------------------"
         cat ./pdfs/texlive-lualatex_tr.txt
-        echo "\n----------------------"
+        echo -e "\n----------------------"
+        echo "Differences with referential text:"
         diff ./pdfs/texlive-lualatex_tr.txt ./releases/referential/referential-lualatex.txt
+        echo "All done!"
         ;;
+    
     pdf)
         pdftotext ./pdfs/texlive-pdflatex.pdf ./pdfs/texlive-pdflatex.txt
         cat ./pdfs/texlive-pdflatex.txt | tr -d "[:space:]" > ./pdfs/texlive-pdflatex_tr.txt
         echo "Generated pdf:"
         echo "----------------------"
         cat ./pdfs/texlive-pdflatex_tr.txt
-        echo "\n----------------------"
+        echo -e "\n----------------------"
+        echo "Differences with referential text:"
         diff ./pdfs/texlive-pdflatex_tr.txt ./releases/referential/referential-pdflatex.txt
+        echo "All done!"
         ;;
+    
     xetex)
         pdftotext ./pdfs/texlive-xelatex.pdf ./pdfs/texlive-xelatex.txt
         cat ./pdfs/texlive-xelatex.txt | tr -d "[:space:]" > ./pdfs/texlive-xelatex_tr.txt
         echo "Generated pdf:"
         echo "----------------------"
         cat ./pdfs/texlive-xelatex_tr.txt
-        echo "\n----------------------"
+        echo -e "\n----------------------"
+        echo "Differences with referential text:"
         diff ./pdfs/texlive-xelatex_tr.txt ./releases/referential/referential-xelatex.txt
+        echo "All done!"
         ;;
+    
     *)
         echo Unknown LaTeX compiler!
         exit 1
         ;;
+    
 esac

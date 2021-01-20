39b1094129b8787933e2d04f62a25b8fe4aff971
Artur M. Brodzki <artur@brodzki.org>
2021-01-20 20:14:31 +0100

    generate tr-ed referential

--- scripts/verify-build.sh
+++ scripts/verify-build.sh
@@ -5,18 +5,24 @@ set -e
 case $1 in
     lua)
         pdftotext ./pdfs/texlive-lualatex.pdf ./pdfs/texlive-lualatex.txt
-        cat ./pdfs/texlive-lualatex.txt
-        diff ./pdfs/texlive-lualatex.txt ./releases/referential/referential-lualatex.txt
+        cat ./pdfs/texlive-lualatex.txt | tr -d "[:space:]" > ./pdfs/texlive-lualatex_tr.txt
+        cat ./pdfs/texlive-lualatex_tr.txt
+        echo "----------------------"
+        diff ./pdfs/texlive-lualatex_tr.txt ./releases/referential/referential-lualatex.txt
         ;;
     pdf)
         pdftotext ./pdfs/texlive-pdflatex.pdf ./pdfs/texlive-pdflatex.txt
-        cat ./pdfs/texlive-pdflatex.txt
-        diff ./pdfs/texlive-pdflatex.txt ./releases/referential/referential-pdflatex.txt
+        cat ./pdfs/texlive-pdflatex.txt | tr -d "[:space:]" > ./pdfs/texlive-pdflatex_tr.txt
+        cat ./pdfs/texlive-pdflatex_tr.txt
+        echo "----------------------"
+        diff ./pdfs/texlive-pdflatex_tr.txt ./releases/referential/referential-pdflatex.txt
         ;;
     xetex)
         pdftotext ./pdfs/texlive-xelatex.pdf ./pdfs/texlive-xelatex.txt
-        cat ./pdfs/texlive-xelatex.txt
-        diff ./pdfs/texlive-xelatex.txt ./releases/referential/referential-xelatex.txt
+        cat ./pdfs/texlive-xelatex.txt | tr -d "[:space:]" > ./pdfs/texlive-xelatex_tr.txt
+        cat ./pdfs/texlive-xelatex_tr.txt
+        echo "----------------------"
+        diff ./pdfs/texlive-xelatex_tr.txt ./releases/referential/referential-xelatex.txt
         ;;
     *)
         echo Unknown LaTeX compiler!

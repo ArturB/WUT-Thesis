741f77a4f323383f6ccab97f0c3089270413ff32
Artur M. Brodzki <artur@brodzki.org>
2021-01-20 19:28:51 +0100

    verbose cmp

--- scripts/verify-build.sh
+++ scripts/verify-build.sh
@@ -5,15 +5,15 @@ set -e
 case $1 in
     lua)
         pdftotext ./pdfs/texlive-lualatex.pdf ./pdfs/texlive-lualatex.txt
-        cmp ./pdfs/texlive-lualatex.txt ./releases/referential/referential-lualatex.txt
+        cmp -l ./pdfs/texlive-lualatex.txt ./releases/referential/referential-lualatex.txt
         ;;
     pdf)
         pdftotext ./pdfs/texlive-pdflatex.pdf ./pdfs/texlive-pdflatex.txt
-        cmp ./pdfs/texlive-pdflatex.txt ./releases/referential/referential-pdflatex.txt
+        cmp -l ./pdfs/texlive-pdflatex.txt ./releases/referential/referential-pdflatex.txt
         ;;
     xetex)
         pdftotext ./pdfs/texlive-xelatex.pdf ./pdfs/texlive-xelatex.txt
-        cmp ./pdfs/texlive-xelatex.txt ./releases/referential/referential-xelatex.txt
+        cmp -l ./pdfs/texlive-xelatex.txt ./releases/referential/referential-xelatex.txt
         ;;
     *)
         echo Unknown LaTeX compiler!

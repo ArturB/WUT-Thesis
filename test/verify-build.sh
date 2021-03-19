938e84d26c4ff57de3df6bf2b32996a938e0da49
Artur M. Brodzki <artur@brodzki.org>
2021-03-19 23:24:19 +0100

    new referentials

--- test/verify-build.sh
+++ test/verify-build.sh
@@ -10,39 +10,39 @@ case $1 in
 
     lua)
         pdftotext $PDFS_DIR/$NAME_PREFIX-lualatex.pdf $PDFS_DIR/$NAME_PREFIX-lualatex.txt
-        cat $PDFS_DIR/$NAME_PREFIX-lualatex.txt | tr -d "[:space:]" | tr -s "eE" "\n" > $PDFS_DIR/$NAME_PREFIX-lualatex_tr.txt
+        cat $PDFS_DIR/$NAME_PREFIX-lualatex.txt | tr -d "[:space:]" | tr -d "[:digit:]" | tr -d '-' | tr -d '.' | tr -s "eE" "\n" > $PDFS_DIR/$NAME_PREFIX-lualatex_tr.txt
         echo "Generated pdf:"
         echo "----------------------"
         cat $PDFS_DIR/$NAME_PREFIX-lualatex_tr.txt
         echo -e "\n----------------------"
         echo -e "Differences with referential text:\n"
-        diff $PDFS_DIR/$NAME_PREFIX-lualatex_tr.txt $REFERENTIAL_DIR/referential-lualatex_tr.txt
+        diff $PDFS_DIR/$NAME_PREFIX-lualatex_tr.txt $REFERENTIAL_DIR/main-lualatex.pdf_referential.txt
         rm $PDFS_DIR/*.txt
         echo "All done!"
         ;;
     
     pdf)
         pdftotext $PDFS_DIR/$NAME_PREFIX-pdflatex.pdf $PDFS_DIR/$NAME_PREFIX-pdflatex.txt
-        cat $PDFS_DIR/$NAME_PREFIX-pdflatex.txt | tr -d "[:space:]" | tr -s "eE" "\n" > $PDFS_DIR/$NAME_PREFIX-pdflatex_tr.txt
+        cat $PDFS_DIR/$NAME_PREFIX-pdflatex.txt | tr -d "[:space:]" | tr -d "[:digit:]" | tr -d '-' | tr -d '.' | tr -s "eE" "\n" > $PDFS_DIR/$NAME_PREFIX-pdflatex_tr.txt
         echo "Generated pdf:"
         echo "----------------------"
         cat $PDFS_DIR/$NAME_PREFIX-pdflatex_tr.txt
         echo -e "\n----------------------"
         echo -e "Differences with referential text:\n"
-        diff $PDFS_DIR/$NAME_PREFIX-pdflatex_tr.txt $REFERENTIAL_DIR/referential-pdflatex_tr.txt
+        diff $PDFS_DIR/$NAME_PREFIX-pdflatex_tr.txt $REFERENTIAL_DIR/main-pdflatex.pdf_referential.txt
         rm $PDFS_DIR/*.txt
         echo "All done!"
         ;;
     
     xetex)
         pdftotext $PDFS_DIR/$NAME_PREFIX-xelatex.pdf $PDFS_DIR/$NAME_PREFIX-xelatex.txt
-        cat $PDFS_DIR/$NAME_PREFIX-xelatex.txt | tr -d "[:space:]" | tr -s "eE" "\n" > $PDFS_DIR/$NAME_PREFIX-xelatex_tr.txt
+        cat $PDFS_DIR/$NAME_PREFIX-xelatex.txt | tr -d "[:space:]" | tr -d "[:digit:]" | tr -d '-' | tr -d '.' | tr -s "eE" "\n" > $PDFS_DIR/$NAME_PREFIX-xelatex_tr.txt
         echo "Generated pdf:"
         echo "----------------------"
         cat $PDFS_DIR/$NAME_PREFIX-xelatex_tr.txt
         echo -e "\n----------------------"
         echo -e "Differences with referential text:\n"
-        diff $PDFS_DIR/$NAME_PREFIX-xelatex_tr.txt $REFERENTIAL_DIR/referential-xelatex_tr.txt
+        diff $PDFS_DIR/$NAME_PREFIX-xelatex_tr.txt $REFERENTIAL_DIR/main-xelatex.pdf_referential.txt
         rm $PDFS_DIR/*.txt
         echo "All done!"
         ;;
